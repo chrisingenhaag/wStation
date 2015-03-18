27d4841fe03524151823f0b70b29cf02490d26b6
Christian Ingenhaag <christian.ingenhaag@westernacher.com>
2015-03-18 19:48:57 +0100

    fix repo for sql.date usage

--- src/main/java/tinker/data/SensorStateRepository.java
+++ src/main/java/tinker/data/SensorStateRepository.java
@@ -1,8 +1,8 @@
 package tinker.data;
 
+import java.sql.Date;
 import java.util.List;
 
-import org.joda.time.DateTime;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
@@ -14,9 +14,9 @@ import tinker.data.entities.SensorState;
 @RepositoryRestResource(collectionResourceRel = "sensorState", path = "sensorState")
 public interface SensorStateRepository extends PagingAndSortingRepository<SensorState, Long> {
 
-	public List<SensorState> findByCreatedDateBetween(@Param("start") @DateTimeFormat(pattern="yyyy-MM-dd-HH-mm") DateTime start, @Param("end") @DateTimeFormat(pattern="yyyy-MM-dd-HH-mm") DateTime end);
+	public List<SensorState> findByCreatedDateBetween(@Param("start") @DateTimeFormat(pattern="yyyy-MM-dd-HH-mm") Date start, @Param("end") @DateTimeFormat(pattern="yyyy-MM-dd-HH-mm") Date end);
 	
 	@Query("select avg(x.temperature) from SensorState x where x.createdDate between :start and :end")
-	public double getAverageTemperatureBetween(@Param("start") @DateTimeFormat(pattern="yyyy-MM-dd-HH-mm") DateTime start, @Param("end") @DateTimeFormat(pattern="yyyy-MM-dd-HH-mm") DateTime end);
+	public double getAverageTemperatureBetween(@Param("start") @DateTimeFormat(pattern="yyyy-MM-dd-HH-mm") Date start, @Param("end") @DateTimeFormat(pattern="yyyy-MM-dd-HH-mm") Date end);
 	
 }
