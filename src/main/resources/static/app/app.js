551fb302f022ae77554df9d64d59138563715915
Christian Ingenhaag <christian.ingenhaag@westernacher.com>
2015-03-21 10:05:37 +0100

    implemented simple navagation and routes

--- src/main/resources/static/app/app.js
+++ src/main/resources/static/app/app.js
@@ -1,3 +1,49 @@
-(function(angular) {
-  angular.module("wStation", ['wStation.services','wStation.controllers']);
-}(angular));
+var wStationApp = angular.module("wStation", [ 'ngRoute', 'wStation.services',
+		'wStation.controllers' ]);
+
+wStationApp.config([ '$routeProvider', function($routeProvider) {
+	$routeProvider.when('/list', {
+		templateUrl : './partials/sensorStateList.html',
+		controller : 'SensorStateListCtrl',
+		name: 'Sensorhistorie'
+	}).otherwise({
+		redirectTo : '/list'
+	});
+} ]);
+
+wStationApp.run(function($rootScope, $location, $route) {
+	$rootScope.$location = $location;
+	$rootScope.$route = $route;
+	$rootScope.keys = Object.keys;
+});
+
+wStationApp.factory('routeNavigation', function($route, $location) {
+	var routes = [];
+	angular.forEach($route.routes, function(route, path) {
+		if (route.name) {
+			routes.push({
+				path : path,
+				name : route.name
+			});
+		}
+	});
+	console.log($route.routes);
+	return {
+		routes : routes,
+		activeRoute : function(route) {
+			return route.path === $location.path();
+		}
+	};
+});
+
+wStationApp.directive('navigation', function(routeNavigation) {
+	return {
+		restrict : "E",
+		replace : true,
+		templateUrl : "./directives/navigation-directive.tpl.html",
+		controller : function($scope) {
+			$scope.routes = routeNavigation.routes;
+			$scope.activeRoute = routeNavigation.activeRoute;
+		}
+	};
+});
--- src/main/resources/static/app/services.js
+++ src/main/resources/static/app/services.js
@@ -1,10 +1,15 @@
-var wStationServices = angular.module('wStation.services', ['ngResource']);
+var wStationServices = angular.module('wStation.services', [ 'ngResource' ]);
 
-wStationServices.factory('SensorState', ['$resource',
-function($resource){
-    //return $resource('http://192.168.2.11:8080/sensorState', {},{
-    return $resource('/sensorState', {},{	
-        query: {method:'GET', params: {'sort': 'createdDate,asc', 'size':'10', 'page':'1'}}
-    });
-}
-]);
+wStationServices.factory('SensorState', [ '$resource', function($resource) {
+	// return $resource('http://192.168.2.11:8080/sensorState', {},{
+	return $resource('/sensorState', {}, {
+		query : {
+			method : 'GET',
+			params : {
+				'sort' : 'createdDate,asc',
+				'size' : '10',
+				'page' : '1'
+			}
+		}
+	});
+} ]);
