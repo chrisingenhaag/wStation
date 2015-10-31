d8a96a9190b26ba0e34f3a0b8946fd1ae33964c1
Christian Ingenhaag <gitlab@ingenhaag-tech.de>
2015-10-31 02:52:08 +0100

    some titles moved directive navigation

--- src/main/resources/static/app/app.js
+++ src/main/resources/static/app/app.js
@@ -50,15 +50,3 @@ wStationApp.factory('routeNavigation', function($route, $location) {
 		}
 	};
 });
-
-wStationApp.directive('navigation', function(routeNavigation) {
-	return {
-		restrict : "E",
-		replace : true,
-		templateUrl : "./directives/navigation-directive.tpl.html",
-		controller : function($scope) {
-			$scope.routes = routeNavigation.routes;
-			$scope.activeRoute = routeNavigation.activeRoute;
-		}
-	};
-});
--- src/main/resources/static/app/directives.js
+++ src/main/resources/static/app/directives.js
@@ -1,2 +1,13 @@
 var wStationDirectives = angular.module('wStation.directives',[ 'wStation.services' ]);
 
+wStationApp.directive('navigation', function(routeNavigation) {
+	return {
+		restrict : "E",
+		replace : true,
+		templateUrl : "./directives/navigation-directive.tpl.html",
+		controller : function($scope) {
+			$scope.routes = routeNavigation.routes;
+			$scope.activeRoute = routeNavigation.activeRoute;
+		}
+	};
+});
