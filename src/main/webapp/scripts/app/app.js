87249a15d0a3fe905f3deed0daa38ea3ede7e15b
Christian Ingenhaag <gitlab@ingenhaag-tech.de>
2015-12-10 19:40:12 +0100

    fixed datepicker and d3 charts

--- src/main/webapp/scripts/app/app.js
+++ src/main/webapp/scripts/app/app.js
@@ -2,7 +2,8 @@
 
 angular.module('wStationApp', ['LocalStorageModule', 'tmh.dynamicLocale', 'pascalprecht.translate', 
     'ngResource', 'ngCookies', 'ngAria', 'ngCacheBuster', 'ngFileUpload',
-    'ui.bootstrap', 'ui.router',  'infinite-scroll', 'angular-loading-bar'])
+    'ui.bootstrap', 'ui.router',  'infinite-scroll', 'angular-loading-bar',
+    'nvd3ChartDirectives'])
 
     .run(function ($rootScope, $location, $window, $http, $state, $translate, Language, Auth, Principal, ENV, VERSION) {
         // update the window title using params in the following
