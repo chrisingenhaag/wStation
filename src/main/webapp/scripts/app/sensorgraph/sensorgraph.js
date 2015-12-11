83cf8954abf96515f6a04bcaafd9fc80b9fb9dac
Christian Ingenhaag <gitlab@ingenhaag-tech.de>
2015-12-11 21:22:35 +0100

    fixed translation provider for sensorgraph controller

--- src/main/webapp/scripts/app/sensorgraph/sensorgraph.js
+++ src/main/webapp/scripts/app/sensorgraph/sensorgraph.js
@@ -18,6 +18,7 @@ angular.module('wStationApp')
                 resolve: {
                     mainTranslatePartialLoader: ['$translate', '$translatePartialLoader', function ($translate,$translatePartialLoader) {
                         $translatePartialLoader.addPart('sensorgraph');
+                        $translatePartialLoader.addPart('sensorState');
                         return $translate.refresh();
                     }]
                 }
